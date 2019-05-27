47d9b899c7ce974117d0a464bc2a1a10e73d51ff
lionem2018 <lionem2018@gmail.com>
2019-05-27 15:05:14 +0900

    add comments

--- tools/hangul-skeleton-generator.py
+++ tools/hangul-skeleton-generator.py
@@ -43,11 +43,12 @@ def generate_skeleton_images(labels_csv, label_file, fonts_image_dir, output_dir
     """
 
     # Open the labels file from image-data of hangul images
+    # 각 이미지의 레이블 정보가 담긴 csv 파일과 모든 레이블이 담긴 txt 파일을 엶
     labels_csv = io.open(labels_csv, 'r', encoding='utf-8')
-    labels_file = io.open(label_file, 'r',
-                      encoding='utf-8').read().splitlines()
+    labels_file = io.open(label_file, 'r', encoding='utf-8').read().splitlines()
 
     # Map characters to indices.
+    # 레이블 글자(ex: '가')와 인덱스(ex: 1)를 맵핑
     label_dict = {}
     count = 0
     for label in labels_file:
@@ -55,25 +56,32 @@ def generate_skeleton_images(labels_csv, label_file, fonts_image_dir, output_dir
         count += 1
 
     # Build the lists.
+    # 각 이미지에 대한 레이블 정보를 인덱스로 변경한 레이블 리스트를 생성
     labels = []
     for row in labels_csv:
         _, label = row.strip().split(',')
         labels.append(label_dict[label])
 
-    # Set the path of skeleton images in output directory. It will be used later for 
+    # Set the path of skeleton images in output directory. It will be used later for
     # setting up skeleton images path for skeleton labels
+    # output 디렉토리 안에 스켈레톤 이미지 경로를 설정 (스케렐톤 이미지가 저장될 경로)
+    # 나중에 스켈레톤 레이블을 위한 스켈레톤 이미지 경로를 설정하기 위해 사용될 것임
     image_dir = os.path.join(output_dir, 'skeleton-images')
     if not os.path.exists(image_dir):
         os.makedirs(os.path.join(image_dir))
 
-    # Get the list of all the Hangul images. Sorted function is used to order the arbitray 
-    # output of glob() which is always arbitray
+    # Get the list of all the Hangul images. Sorted function is used to order the arbitrary
+    # output of glob() which is always arbitrary
+    # 모든 한글 이미지에 대한 리스트를 가져옴
+    # 정렬 함수는 항상 임의적인 glob() 임의 출력을 정렬하는데 사용함
     font_images = glob.glob(os.path.join(fonts_image_dir, '*.png'))
     # check if the images are jpeg
+    # png 형식으로 읽어들인 이미지가 없다면, 이미지들이 jpeg 형식인지 확인
     if len(font_images) == 0:
         font_images = glob.glob(os.path.join(fonts_image_dir, '*.jpg'))
 
     # If input directory is empty or no images are found with .png, jpeg and .jpg extension
+    # 입력 디렉토리가 비어있거나 png, jpeg, jpg 확장자를 가진 이미지가 없는 경우 예외처리
     if len(font_images) == 0:
         raise Exception("input_dir contains no image files")
 
@@ -83,6 +91,7 @@ def generate_skeleton_images(labels_csv, label_file, fonts_image_dir, output_dir
 
     # If the image names are numbers, sort by the value rather than asciibetically
     # having sorted inputs means that the outputs are sorted in test mode
+    # 파일 이름 정렬
     if all(get_name(path).isdigit() for path in font_images):
         font_images = sorted(font_images, key=lambda path: int(get_name(path)))
     else:
@@ -91,16 +100,19 @@ def generate_skeleton_images(labels_csv, label_file, fonts_image_dir, output_dir
     #font_images = sorted(font_images, key=lambda x:float(re.findall("(\d+)",x)[0]))
 
     # Create the skeleton labels file
-    labels_csv = io.open(os.path.join(output_dir, 'skeleton-labels-map.txt'), 'w',
-                        encoding='utf-8')
+    # 스켈레톤 레이블 파일(경로와 레이블을 맵핑한 것)을 생성
+    labels_csv = io.open(os.path.join(output_dir, 'skeleton-labels-map.txt'), 'w', encoding='utf-8')
 
     # Set the count so that we can view the progress on the terminal
+    # 터미널에서 진전을 보기위해 카운트 설정
     total_count = 0
-    prev_count= 0
+    prev_count = 0
     index = 0
 
+    # 폰트 이미지들 하나씩 스켈레톤 수행
     for font_image in font_images:
         # Print image count roughly every 5000 images.
+        # 매 5000번째 이미지 쯤에서 이미지 카운트 출력
         if total_count - prev_count > 5000:
             prev_count = total_count
             print('{} skeleton images generated...'.format(total_count))
@@ -111,23 +123,28 @@ def generate_skeleton_images(labels_csv, label_file, fonts_image_dir, output_dir
         # Convert them from rgb to gray and then convert it into bool
         # Then apply skeletonize function and wrap it into binary_closing
         # for converting them into binary
-
+        # 폰트 이미지 디렉토리 부터 이미지를 하나씩 읽어들임
+        # RGB의 이미지를 그레이스케일로 변형한 뒤, bool 형으로 다시 변형 (이미지 이진화 과정)
+        # 골격화 함수를 적용하고 binary_closing 수행(이진 모폴로지 닫기 수행 - 이미지 사이 빈 부분 채우기 위해)
         image = img_as_bool(rgb2gray(imread(font_image)))
         skeleton = binary_closing(thin(image))
 
         # convert image as uint before saving in output directory
+        # 출력 디렉토리에 저장하기 전에 uint로 이미지를 변환
         skeleton = img_as_uint(skeleton)
 
+        # 이미지 저장
         file_string = '{}.png'.format(total_count)
         file_path = os.path.join(image_dir, file_string)     
         ioo.imsave(fname=file_path, arr=skeleton)
 
         # Using Labels list and label_dict dictionary we construct the skeleton labels
         # In skeleton label csv file
+        # 레이블 리스트와 레이블 딕셔너리를 사용하여 스켈레톤 레이블 작성
+        # 스켈레톤 레이블 csv(여기서는 txt 형식)에 저장
         character = list(label_dict.keys())[labels[index]]
         labels_csv.write(u'{},{}\n'.format(file_path, character))
         index += 1    
-
 
 
 if __name__ == '__main__':
--- tools/preprocess.py
+++ tools/preprocess.py
@@ -11,26 +11,31 @@ import numpy as np
 import threading
 
 
-# setting global variable for counter 
+# setting global variable for counter
+# 카운터를 위한 글로벌 변수 설정
 index = 0
 total_count = 0 
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Default data paths.
-DEFAULT_LABEL_FILE = os.path.join(SCRIPT_PATH,
-                                  '../labels/2350-common-hangul.txt')
+# 디폴드 데이터 경로
+DEFAULT_LABEL_FILE = os.path.join(SCRIPT_PATH, '../labels/2350-common-hangul.txt')
 DEFAULT_FONTS_IMAGE_DIR = os.path.join(SCRIPT_PATH, '../image-data/hangul-images')
+DEFAULT_SKELETON_IMAGE_DIR = os.path.join(SCRIPT_PATH, '../skeleton-image-data/skeleton-images')
 DEFAULT_OUTPUT_DIR = os.path.join(SCRIPT_PATH, '../hangul-skeleton-combine-images')
-
 DEFAULT_LABEL_CSV = os.path.join(SCRIPT_PATH, '../image-data/labels-map.csv')
 
 
+# 하나의 한글 이미지 정보를 입력 받아 스켈레톤 이미지를 찾아 전처리한 후 나란히 연결함
 def combine(src, src_path):
     if args.b_dir is None:
         raise Exception("missing b_dir")
 
     # find corresponding file in b_dir, could have a different extension
+    # b_dir에서 해당 파일 탐색, 다른 확장자를 가질 수도 있음
+    # 입력받은 한글 이미지(src_path)의 확장자를 뺀 이름만 가져와
+    # 해당 이름을 가진 스켈레톤 이미지의 경로를 얻음(여기서는 sibling-형제-으로 표현)
     basename, _ = os.path.splitext(os.path.basename(src_path))
     for ext in [".png", ".jpg"]:
         sibling_path = os.path.join(args.b_dir, basename + ext)
@@ -41,11 +46,13 @@ def combine(src, src_path):
         raise Exception("could not find sibling image for " + src_path)
 
     # make sure that dimensions are correct
+    # 한글 이미지의 크기와 스켈레톤 이미지의 크기가 같은지 확인
     height, width, _ = src.shape
     if height != sibling.shape[0] or width != sibling.shape[1]:
         raise Exception("differing sizes")
     
     # convert both images to RGB if necessary
+    # 두 이미지가 만일 그레이스케일 이미지라면 RGB로 변환
     if src.shape[2] == 1:
         src = im.grayscale_to_rgb(images=src)
 
@@ -53,27 +60,34 @@ def combine(src, src_path):
         sibling = im.grayscale_to_rgb(images=sibling)
 
     # remove alpha channel
+    # 두 이미지의 알파 채널(투명도)를 삭제함
     if src.shape[2] == 4:
         src = src[:,:,:3]
     
     if sibling.shape[2] == 4:
         sibling = sibling[:,:,:3]
 
+    # 두 이미지를 하나로 합쳐 리턴
     return np.concatenate([src, sibling], axis=1)
+
 
 def process(src_path, dst_path, label_dict, labels, labels_csv, image_dir):
     global index
     global total_count
 
+    # 입력받은 하나의 한글 이미지 경로를 가지고 해당 한글 이미지를 읽음
     total_count += 1
     src = im.load(src_path)
 
+    # 명령어에서 인자로 입력받은 연산이 combine이라면 combine 수행
     if args.operation == "combine":
         dst = combine(src, src_path)
     else:
         raise Exception("invalid operation")
+    # combine을 수행한 결과를 파일로 저장함
     im.save(dst, dst_path)
 
+    # 저장한 combine 결과 이미지를 csv 파일에 레이블을 맵핑하여 저장
     file_string = '{}.png'.format(total_count)
     file_path = os.path.join(image_dir, file_string)  
 
@@ -81,10 +95,12 @@ def process(src_path, dst_path, label_dict, labels, labels_csv, image_dir):
     labels_csv.write(u'{},{}\n'.format(file_path, character))
     index += 1 
 
+
 complete_lock = threading.Lock()
 start = None
 num_complete = 0
 total = 0
+
 
 def complete():
     global num_complete, rate, last_complete
@@ -99,21 +115,24 @@ def complete():
         else:
             remaining = 0
 
-        print("%d/%d complete  %0.2f images/sec  %dm%ds elapsed  %dm%ds remaining" % (num_complete, total, rate, elapsed // 60, elapsed % 60, remaining // 60, remaining % 60))
+        print("%d/%d complete  %0.2f images/sec  %dm%ds elapsed  %dm%ds remaining"
+              % (num_complete, total, rate, elapsed// 60, elapsed % 60, remaining // 60, remaining % 60))
 
         last_complete = now
 
 
 def generate_hangul_skeleton_combine_images(labels_csv, label_file, output_dir):
+    # 출력 디렉토리 유무 확인 후 생성
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
     # Open the labels file from image-data of hangul images
+    # 한글 이미지가 있는 image-data 폴더로부터 레이블 파일을 읽어들임
     labels_csv = io.open(labels_csv, 'r', encoding='utf-8')
-    labels_file = io.open(label_file, 'r',
-                      encoding='utf-8').read().splitlines()
+    labels_file = io.open(label_file, 'r', encoding='utf-8').read().splitlines()
 
     # Map characters to indices.
+    # 글자 레이블 정보와 인덱스를 맵핑
     label_dict = {}
     count = 0
     for label in labels_file:
@@ -121,6 +140,7 @@ def generate_hangul_skeleton_combine_images(labels_csv, label_file, output_dir):
         count += 1
 
     # Build the lists.
+    # 각 이미지에 대한 레이블 정보를 인덱스로 변경한 레이블 리스트를 생성
     labels = []
     for row in labels_csv:
         _, label = row.strip().split(',')
@@ -128,19 +148,23 @@ def generate_hangul_skeleton_combine_images(labels_csv, label_file, output_dir):
 
     # Set the path of hangul-skeleton-combine images in output directory. It will be used later for 
     # setting up hangul-skeleton-combine images path for hangul-skeleton-combine labels
+    # output 디렉토리 안에 combine 결과 이미지 경로를 설정 (이미지가 저장될 경로)
+    # 나중에 combine 레이블을 위한 combine 결과 이미지 경로를 설정하기 위해 사용될 것임
     image_dir = os.path.join(output_dir, 'images')
     if not os.path.exists(image_dir):
         os.makedirs(os.path.join(image_dir))
 
     # Create the skeleton labels file
-    labels_csv = io.open(os.path.join(args.output_dir, 'skeleton-labels-map.txt'), 'w',
-                     encoding='utf-8')
+    # combine 결과 이미지 레이블 파일 생성
+    labels_csv = io.open(os.path.join(args.output_dir, 'skeleton-labels-map.txt'), 'w', encoding='utf-8')
 
     src_paths = []
     dst_paths = []
 
-    # Check if the directory and images already exsist?
+    # Check if the directory and images already exist?
     # If yes then skip those images else create the paths list
+    # combine 결과 이미지가 이미 존재하는 지 확인
+    # 만약 존재한다면 그 이미지들을 스킵하고, 그렇지 않으면 경로 리스트를 생성함
     skipped = 0
     for src_path in sorted(im.find(args.input_dir)):
         name, _ = os.path.splitext(os.path.basename(src_path))
@@ -171,16 +195,18 @@ def generate_hangul_skeleton_combine_images(labels_csv, label_file, output_dir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", required=True, 
+    parser.add_argument("--input_dir",
+                            default=DEFAULT_FONTS_IMAGE_DIR,
                             help="path to folder containing images")
     parser.add_argument('--output-dir', type=str, dest='output_dir',
                             default=DEFAULT_OUTPUT_DIR,
                             help='Output directory to store generated hangul skeleton images and '
                                  'label CSV file.')
-    parser.add_argument("--operation", required=True, choices=["combine"])
+    parser.add_argument("--operation", default='combine', choices=["combine"])
     parser.add_argument("--workers", type=int, default=1, help="number of workers")
     # combine
-    parser.add_argument("--b_dir", type=str, help="path to folder containing B images for combine operation")
+    parser.add_argument("--b_dir", type=str, default=DEFAULT_SKELETON_IMAGE_DIR,
+                        help="path to folder containing B images for combine operation")
     # labels
     parser.add_argument('--image-label-csv', type=str, dest='labels_csv',
                         default=DEFAULT_LABEL_CSV,
